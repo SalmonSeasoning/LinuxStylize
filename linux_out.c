b3f0b207d777f05e3d6199b301d7d336f3c4dbec
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2020-03-21 17:18:18 -0500

    Update linux_out.c

--- linux_out.c
+++ linux_out.c
@@ -1,3 +1,8 @@
+////////////////////////////////////////////////////
+//             (C) 2020 Ty Qualters               //
+//      See LICENSE for license information       //
+////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
